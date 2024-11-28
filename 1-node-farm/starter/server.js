79c66937d087721bba000e052968eb85ad7bbad6
Durgesh Prasad <durgeshhh.in@gmail.com>
2024-11-28 22:25:00 +0530

    1.4 Using 3rd party modules slugify

--- 1-node-farm/starter/server.js
+++ 1-node-farm/starter/server.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 const replaceTemplate = require('./modules/replaceTemplate');
+const slugify = require('slugify');
 /////////////////////////////////
 // FILES
 
@@ -47,6 +48,9 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const Dataobj = JSON.parse(data);
+const slugs= Dataobj.map(el => slugify(el.productName,{lower: true}));
+console.log(slugs);
+
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
 
