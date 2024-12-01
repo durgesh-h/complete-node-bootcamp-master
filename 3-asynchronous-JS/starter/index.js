1e5931c78983726196f9e896ff57f9c45c4df778
Durgesh Prasad <durgeshhh.in@gmail.com>
2024-12-02 00:25:53 +0530

    3.6 Used multiple promises simultaneously

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -105,13 +105,21 @@ const getDogPic = async () => {
   try {
     const data = await readfilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
-
-    const res = await superagent.get(
+    // Used multiple promises simultaneously
+    const res1Pro = superagent.get(
       `https://dog.ceo/api/breed/${data}/images/random`
     );
-    console.log(res.body.message);
+    const res2Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res3Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    const imgs = all.map((el) => el.body.message);
+    console.log(imgs);
 
-    await writefilePro("dog-img.txt", res.body.message);
+    await writefilePro("dog-img.txt", imgs.join("\n"));
     console.log("Random dog image saved to file!👍");
   } catch (err) {
     console.log(err);
