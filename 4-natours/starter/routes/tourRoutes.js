bb436a2ec42b76130d01c8959be522276eebf268
Durgesh Prasad <durgeshhh.io@gmail.com>
2024-12-10 00:25:49 +0530

    5.5 chaining multiple midleware

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -7,12 +7,13 @@ const {
   updateTour,
   deleteTour,
   checkID,
+  checkBody,
 } = require('../controllers/tourController');
 const router = express.Router();
 
 router.param('id', checkID);
 
-router.route('/').get(getAllTour).post(createTour);
+router.route('/').get(getAllTour).post(checkBody, createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
