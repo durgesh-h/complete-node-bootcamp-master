3dea297140c038225900e4bed51b18fce9869ce3
Durgesh Prasad <durgeshhh.io@gmail.com>
2024-12-10 10:59:49 +0530

    5.7 Environment variables

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,28 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-// 1)MIDDLEWARES
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  req.requestedTime = new Date().toISOString();
+  console.log('Hello from the middleware 👋');
   next();
 });
-app.use(morgan('dev'));
-//Serving static file
-app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 // 3) ROUTES
-
-// app.get('/api/v1/tours', getAllTour);
-// app.post('/api/v1/tours', createTour);
-// app.get('/api/v1/tours/:id', getTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
