23bf167ccc1904bbbb71bb7b55da3aa69fc13a5d
Durgesh Prasad <durgeshhh.io@gmail.com>
2024-12-10 13:18:21 +0530

    7.2 Connecting DB with the Express

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE_URL;
 
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB Connection Successful!'));
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
