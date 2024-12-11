916478e9419fb0eb9e07ca368e718a48c48833d9
Durgesh Prasad <durgeshhh.io@gmail.com>
2024-12-11 23:40:32 +0530

    9.3 Aggregation Pipeline: Unwinding and Projecting

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -8,10 +8,12 @@ const {
   deleteTour,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlan,
 } = require('../controllers/tourController');
 const router = express.Router();
 
 // router.param('id', checkID);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/tour-stats').get(getTourStats);
 router.route('/top-5-cheap').get(aliasTopTours, getAllTour);
 router.route('/').get(getAllTour).post(createTour);
