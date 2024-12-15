b197673296e50dc37960302b06e35d7b82c640a0
Durgesh Prasad <durgeshhh.io@gmail.com>
2024-12-15 22:07:46 +0530

    11.5 Better Errors and refactoring

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appErros');
 
 const app = express();
 
@@ -30,23 +31,11 @@ app.use('/api/v1/users', userRouter);
 
 // Handing Unhandled routes
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server! `,
-  // });
-  const err = new Error(`Can't find ${req.originalUrl} on this server! `);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  // const err = new Error(`Can't find ${req.originalUrl} on this server! `);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  next(new AppError(`Can't find ${req.originalUrl} on this server! `));
 });
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'Error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
