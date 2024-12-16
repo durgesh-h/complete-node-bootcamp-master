5238a926d9a769932c4ae324db5c27dd9524bab4
Durgesh Prasad <durgeshhh.io@gmail.com>
2024-12-16 21:56:34 +0530

    12.5 Modelling users

---
+++ 4-natours/starter/models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    isLowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email!'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a Password'],
+    minLength: 8,
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, 'Please provide a Password'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
